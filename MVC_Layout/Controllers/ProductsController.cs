32954253d8a8423fb2a75d64efa6cf492471d3b1
muratvuranok <mrtvrnk@gmail.com>
2019-06-24 20:23:00 +0300

    project initialize

---
+++ MVC_Layout/Controllers/ProductsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Layout.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
