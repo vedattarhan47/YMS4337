244897c9dd41049b6ede361e06ab7056ef6d97eb
muratvuranok <mrtvrnk@gmail.com>
2019-06-25 20:54:03 +0300

    end of lesson

--- MVC_Layout/Areas/Admin/Controllers/CategoriesController.cs
+++ MVC_Layout/Areas/Admin/Controllers/CategoriesController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MVC_Layout.Areas.Admin.Models;
 namespace MVC_Layout.Areas.Admin.Controllers
 {
     public class CategoriesController : Controller
     {
-        // GET: Admin/Categories
+        NorthwindEntities db = new NorthwindEntities();
         public ActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult _Index()
+        {
+            var categories = db.Categories.Select(x => new
+            {
+                x.CategoryID,
+                x.CategoryName,
+                x.Description
+            }).ToList();
+            return Json(categories, JsonRequestBehavior.AllowGet);
         }
     }
 }
